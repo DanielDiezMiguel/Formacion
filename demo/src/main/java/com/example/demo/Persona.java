36217d9013da008c99b2906e4d1a6358a586e676
DanielDiezMiguel <daniel.diez@bosonit.com>
2022-07-12 15:19:42 +0200

    Ejercicio Spring terminado

--- demo/src/main/java/com/example/demo/Persona.java
+++ demo/src/main/java/com/example/demo/Persona.java
@@ -1,29 +1,10 @@
 package com.example.demo;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @Data
-@AllArgsConstructor
-@RestController()
 public class Persona {
     String name;
-    String edad;
+    Integer edad;
     String ciudad;
-
-    @PostMapping("/useradd")
-    public Persona getPersona(@PathVariable String name, String edad, String ciudad) {
-        return new Persona(name, edad + 1, ciudad);
-    }
-
-    @GetMapping("/user/{nombre}")
-    public String getHola(@PathVariable String nombre){
-
-        return "Hola: " + nombre;
-    }
-
 }
