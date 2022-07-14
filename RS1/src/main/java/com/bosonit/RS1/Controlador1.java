32bd5a0a109e1e51b66ff8455d56e3ef537e5745
DanielDiezMiguel <daniel.diez@bosonit.com>
2022-07-14 10:28:36 +0200

    Ejercicio RS1 terminado

---
+++ RS1/src/main/java/com/bosonit/RS1/Controlador1.java
@@ -0,0 +1,38 @@
+package com.bosonit.RS1;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+@RestController
+public class Controlador1 {
+
+//    private static final String template = "Hello, %s";
+//    private final AtomicLong contador = new AtomicLong();
+//
+//    @GetMapping("/greeting")
+//    public Greeting getInfoGreeting(@RequestParam(value = "name", defaultValue = "World") String name){
+//
+//        return new Greeting(contador.incrementAndGet(), String.format(template, name));
+//    }
+
+    @PostMapping("/greeting")
+    public Greeting getInfoGreeting(@RequestBody Greeting g){
+        return g;
+    }
+
+    @GetMapping("/user/{id}")
+    public Greeting getInfoGreetingID(@RequestBody Greeting g, @PathVariable long id){
+        g.setId(id);
+        return g;
+    }
+
+    @PutMapping("/post")
+    public String postVar(@RequestParam(value = "var1") int var1,
+            @RequestParam(value = "var2") int var2
+    )
+    {
+        return "Value1:" + var1 + " Var2:" + var2;
+    }
+
+}
---
+++ RS1/src/main/java/com/bosonit/RS1/Greeting.java
@@ -0,0 +1,9 @@
+package com.bosonit.RS1;
+
+import lombok.Data;
+
+@Data
+public class Greeting {
+    private long id;
+    private String content;
+}
