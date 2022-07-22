9a509dec974ea7c8675b702e035e0537a53e49c5
DanielDiezMiguel <daniel.diez@bosonit.com>
2022-07-22 10:41:16 +0200

    BP-1 Arquitectura Hexagonal
    Ejercicio terminado

--- BP1-ArquitecturaHexagonal/src/main/java/com/bosonit/application/DeletePersonaUserCase.java
+++ BP1-ArquitecturaHexagonal/src/main/java/com/bosonit/application/DeletePersonaUserCase.java
@@ -6,14 +6,16 @@ import com.bosonit.infrastructure.controller.dto.input.UsuarioInputDTO;
 import com.bosonit.infrastructure.controller.dto.output.UsuarioOutputDTO;
 import com.bosonit.infrastructure.repository.jpa.UsuarioRepositorio;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 @Service
 public class DeletePersonaUserCase implements DeletePersonaPort {
     @Autowired
     UsuarioRepositorio usuarioRepositorio;
     public void deleteByID(Integer id) {
-        UsuarioEntity usuarioEntity = usuarioRepositorio.findById(id).orElseThrow(() -> new RuntimeException("No se ha encontrado el ID"));
+        UsuarioEntity usuarioEntity = usuarioRepositorio.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_ACCEPTABLE));
         usuarioRepositorio.delete(usuarioEntity);
         usuarioRepositorio.save(usuarioEntity);
     }
