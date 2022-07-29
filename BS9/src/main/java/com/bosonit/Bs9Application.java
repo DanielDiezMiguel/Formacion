0c33a9bc8c999fb1c346ba273e4852f769dbd580
DanielDiezMiguel <daniel.diez@bosonit.com>
2022-07-29 16:16:26 +0200

    BS9
    Proyecto creado

---
+++ BS9/src/main/java/com/bosonit/Bs9Application.java
@@ -0,0 +1,13 @@
+package com.bosonit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bs9Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bs9Application.class, args);
+	}
+
+}
---
+++ BS9/src/main/java/com/bosonit/controller/ControladorPersona.java
@@ -0,0 +1,58 @@
+package com.bosonit.controller;
+
+import com.bosonit.dto.UsuarioInputDTO;
+import com.bosonit.dto.UsuarioOutputDTO;
+import com.bosonit.model.UsuarioEntity;
+import com.bosonit.repository.UsuarioRepositorio;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+public class ControladorPersona {
+
+    @Autowired
+    UsuarioRepositorio usuarioRepositorio;
+
+    @PostMapping("/crearUsuario")
+    public UsuarioOutputDTO crearUsuario(@RequestBody UsuarioInputDTO usuarioInputDTO, UsuarioEntity usuarioEntity, UsuarioOutputDTO usuarioOutputDTO) throws Exception {
+        if (usuarioInputDTO.getUsuario().length() < 6 || usuarioInputDTO.getUsuario().length() > 10)
+            throw new Exception("El usuario debe tener entre 6 y 10 carácteres");
+
+        usuarioEntity = new UsuarioEntity(usuarioInputDTO);
+        usuarioRepositorio.save(usuarioEntity);
+        usuarioOutputDTO = new UsuarioOutputDTO(usuarioEntity);
+        return usuarioOutputDTO;
+
+    }
+
+    @GetMapping("/id/{id}")
+    public Optional<UsuarioEntity> getUsuarioByID(@PathVariable(value = "id") Integer id) {
+        return usuarioRepositorio.findById(id);
+    }
+
+
+    @GetMapping("/usuario/{usuario}")
+    public List<UsuarioEntity> getUsuarioByUser(@PathVariable(value = "usuario") String usuario) {
+        return usuarioRepositorio.findByUsuario(usuario);
+    }
+
+    @GetMapping("/usuario/all")
+    public List<UsuarioEntity> getAllUsuarios() {
+        return usuarioRepositorio.findAll();
+    }
+
+    @PutMapping("/usuario/update/{id}")
+    public void updateUsuarioByID(@PathVariable(value = "id") Integer id) {
+        UsuarioEntity usuarioEntity1 = usuarioRepositorio.getReferenceById(id);
+        usuarioEntity1.setUsuario("PedroRL");
+        usuarioRepositorio.save(usuarioEntity1);
+    }
+
+    @DeleteMapping("/usuario/{id}")
+    public void deleteByID(@PathVariable(value = "id") Integer id) {
+        usuarioRepositorio.deleteById(id);
+    }
+}
