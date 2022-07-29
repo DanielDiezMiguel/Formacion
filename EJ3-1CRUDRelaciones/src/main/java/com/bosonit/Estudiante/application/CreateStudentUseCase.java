6120f05b66bdd1d6a78f03d2ae901a8b2eafbfb2
DanielDiezMiguel <daniel.diez@bosonit.com>
2022-07-29 11:54:30 +0200

    EJ3-1
    Ejercicio terminado

--- EJ3-1CRUDRelaciones/src/main/java/com/bosonit/Estudiante/application/CreateStudentUseCase.java
+++ EJ3-1CRUDRelaciones/src/main/java/com/bosonit/Estudiante/application/CreateStudentUseCase.java
@@ -9,21 +9,16 @@ import com.bosonit.EstudianteAsignatura.domain.EstudianteAsignaturaEntity;
 import com.bosonit.EstudianteAsignatura.infrastructure.repository.jpa.EstudianteAsignaturaRespository;
 import com.bosonit.Persona.domain.PersonaEntity;
 import com.bosonit.Persona.infrastructure.repository.jpa.PersonaRepository;
-import com.bosonit.Profesor.domain.ProfesorEntity;
-import com.bosonit.Profesor.infrastructure.controller.dto.output.ProfesorOutputDTO;
-import jdk.dynalink.linker.LinkerServices;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Set;
 
 @Service
 public class CreateStudentUseCase implements CreateStudentPort {
     @Autowired
     StudentRepository studentRepository;
-
     @Autowired
     EstudianteAsignaturaRespository estudianteAsignaturaRespository;
     @Autowired
@@ -42,31 +37,9 @@ public class CreateStudentUseCase implements CreateStudentPort {
         });
 
         studentEntity.setEstudios(estudianteAsignaturaEntitiesList);
+
         studentRepository.save(studentEntity);
         return new StudentOutputDTO(studentEntity);
 
     }
-
-//    @Override
-//    public StudentOutputDTO crearStudent(StudentInputDTO studentInputDTO) throws Exception {
-//        PersonaEntity personaEntity = personaRepository.findById(studentInputDTO.getId_persona()).orElseThrow(() -> new Exception("No se ha encontrado el ID de Persona"));
-//        ProfesorEntity profesorEntity = profesorRepository.findById(studentInputDTO.getId_profesor()).orElseThrow(() -> new Exception("No se ha encontrado el ID de Profesor"));
-//        StudentEntity studentEntity = new StudentEntity(studentInputDTO);
-//        studentEntity.setPersonaEntity(personaEntity);
-//
-//        // Comienza la comprobación de una persona no puede ser estudiante y profesor
-//        studentEntity.setProfesorEntity(profesorEntity);
-//        if (studentEntity.getProfesorEntity().getId_profesor() != null) {
-//            if (!studentEntity.getPersonaEntity().getId_persona().equals(studentEntity.getProfesorEntity().getId_profesor())) {
-//                studentRepository.save(studentEntity);
-//                return new StudentOutputDTO(studentEntity);
-//            }
-//
-//        } else {
-//            studentRepository.save(studentEntity);
-//            return new StudentOutputDTO(studentEntity);
-//        }
-//
-//        return new StudentOutputDTO(new StudentEntity());
-//    }
 }
