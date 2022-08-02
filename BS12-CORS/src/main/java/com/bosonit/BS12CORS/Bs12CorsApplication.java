8bc79fe2d1ca1ed0f56a55c1bd07a9cb6aa8f4e5
DanielDiezMiguel <daniel.diez@bosonit.com>
2022-08-02 10:34:00 +0200

    BS12-CORS
    Proyecto creado

---
+++ BS12-CORS/src/main/java/com/bosonit/BS12CORS/Bs12CorsApplication.java
@@ -0,0 +1,13 @@
+package com.bosonit.BS12CORS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bs12CorsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bs12CorsApplication.class, args);
+	}
+
+}
---
+++ BS12-CORS/src/test/java/com/bosonit/BS12CORS/Bs12CorsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bosonit.BS12CORS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bs12CorsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
