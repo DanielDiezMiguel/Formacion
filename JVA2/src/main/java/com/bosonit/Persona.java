96e8e8acdedb194095346228ccbee8a796ff9506
DanielDiezMiguel <daniel.diez@bosonit.com>
2022-08-04 18:18:40 +0200

    JVA2
    Ejercicio terminado

--- JVA2/src/main/java/com/bosonit/Persona.java
+++ JVA2/src/main/java/com/bosonit/Persona.java
@@ -1,20 +1,6 @@
 package com.bosonit;
 
-import java.util.Optional;
-
 public record Persona(String nombre, String poblacion, String edad) {
-
-    public Optional<String> getNombre() {
-        return Optional.ofNullable(nombre);
-    }
-
-    public Optional<String> getPoblacion() {
-        return Optional.ofNullable(poblacion);
-    }
-
-    public Optional<String> getEdad() {
-        return Optional.ofNullable(edad);
-    }
 }
 
 
