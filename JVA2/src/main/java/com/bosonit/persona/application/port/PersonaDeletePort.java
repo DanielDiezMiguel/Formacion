cfe47782055ba0ef222610cf2136594a14626647
DanielDiezMiguel <daniel.diez@bosonit.com>
2022-08-04 17:02:00 +0200

    Revert "JVA2"
    This reverts commit 82f15057519fab1d99da9235d2f24fe41d3cc468.

--- JVA2/src/main/java/com/bosonit/persona/application/port/PersonaDeletePort.java
+++
@@ -1,5 +0,0 @@
-package com.bosonit.persona.application.port;
-
-public interface PersonaDeletePort {
-    void deleteByID(Integer id) throws Exception;
-}
--- JVA2/src/main/java/com/bosonit/persona/application/port/PersonaUpdatePort.java
+++
@@ -1,8 +0,0 @@
-package com.bosonit.persona.application.port;
-
-import com.bosonit.persona.infrastructure.controller.dto.PersonaInputDTO;
-import com.bosonit.persona.infrastructure.controller.dto.PersonaOutputDTO;
-
-public interface PersonaUpdatePort {
-    PersonaOutputDTO updatePersonaByID(Integer id, PersonaInputDTO personaInputDTO) throws Exception;
-}
