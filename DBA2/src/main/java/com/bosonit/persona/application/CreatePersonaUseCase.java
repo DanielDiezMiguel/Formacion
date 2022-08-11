ca982660e99faa176bb7093d93e5359f0c97c0c8
DanielDiezMiguel <daniel.diez@bosonit.com>
2022-08-11 18:14:07 +0200

    DBA2
    Ejercicio terminado

--- DBA2/src/main/java/com/bosonit/persona/application/CreatePersonaUseCase.java
+++ DBA2/src/main/java/com/bosonit/persona/application/CreatePersonaUseCase.java
@@ -4,20 +4,20 @@ import com.bosonit.persona.application.port.CreatePersonaPort;
 import com.bosonit.persona.domain.PersonaEntity;
 import com.bosonit.persona.infrastructure.controller.dto.input.PersonaInputDTO;
 import com.bosonit.persona.infrastructure.controller.dto.output.PersonaOutputDTO;
-import com.bosonit.persona.infrastructure.repository.mongo.PersonaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.stereotype.Service;
 
 
 @Service
 public class CreatePersonaUseCase implements CreatePersonaPort {
     @Autowired
-    PersonaRepository personaRepository;
+    MongoTemplate mongoTemplate;
 
     @Override
     public PersonaOutputDTO crearUsuario(PersonaInputDTO personaInputDTO) {
         PersonaEntity personaEntity = new PersonaEntity(personaInputDTO);
-        personaRepository.save(personaEntity);
+        mongoTemplate.save(personaEntity);
         PersonaOutputDTO personaOutputDTO = new PersonaOutputDTO(personaEntity);
         return personaOutputDTO;
 
