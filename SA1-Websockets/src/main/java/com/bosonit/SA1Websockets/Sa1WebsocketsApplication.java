9698228721a9fac77964e6769d97c7415899e475
DanielDiezMiguel <daniel.diez@bosonit.com>
2022-08-12 08:55:02 +0200

    SA1-Websockets
    Ejercicio creado

---
+++ SA1-Websockets/src/main/java/com/bosonit/SA1Websockets/Sa1WebsocketsApplication.java
@@ -0,0 +1,13 @@
+package com.bosonit.SA1Websockets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sa1WebsocketsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sa1WebsocketsApplication.class, args);
+	}
+
+}
---
+++ SA1-Websockets/src/test/java/com/bosonit/SA1Websockets/Sa1WebsocketsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bosonit.SA1Websockets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sa1WebsocketsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
