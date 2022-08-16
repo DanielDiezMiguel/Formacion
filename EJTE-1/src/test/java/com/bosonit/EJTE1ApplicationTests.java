f5940f297de3065dba37e6b675a65679e0c698b5
KosMoS7NS <108921638+DanielDiezMiguel@users.noreply.github.com>
2022-08-16 18:07:42 +0200

    EJTE-1
    Probando tests

--- EJTE-1/src/test/java/com/bosonit/EJTE1ApplicationTests.java
+++ EJTE-1/src/test/java/com/bosonit/EJTE1ApplicationTests.java
@@ -1,13 +1,48 @@
 package com.bosonit;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import com.bosonit.persona.application.port.ReadPersonaPort;
+import com.bosonit.persona.domain.PersonaEntity;
+import com.bosonit.persona.infrastructure.controller.CreatePersonaController;
+import com.bosonit.persona.infrastructure.controller.ReadPersonaController;
+import com.bosonit.persona.infrastructure.controller.dto.output.PersonaOutputDTO;
+import com.bosonit.persona.infrastructure.repository.jpa.PersonaRepository;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.context.event.annotation.BeforeTestClass;
 
-@SpringBootTest
-class EJTE1ApplicationTests {
+import java.util.ArrayList;
+import java.util.List;
 
-	@Test
-	void contextLoads() {
-	}
+@ExtendWith(MockitoExtension.class)
+public class EJTE1ApplicationTests {
 
+    @Mock
+    PersonaRepository personaRepository;
+
+    @InjectMocks
+    CreatePersonaController createPersonaController;
+
+    @InjectMocks
+    ReadPersonaController readPersonaController;
+
+    @BeforeTestClass
+    public void putWhen() {
+        Mockito.when(personaRepository.findAll()).thenReturn(new ArrayList<PersonaEntity>());
+    }
+
+    @Test
+    @DisplayName("Controller using Mockito")
+    public void getAllPersonas() {
+        List<PersonaOutputDTO> personaEntitiesList = readPersonaController.getAllPersonas();
+        Assertions.assertEquals(0, personaEntitiesList.size());
+    }
 }
