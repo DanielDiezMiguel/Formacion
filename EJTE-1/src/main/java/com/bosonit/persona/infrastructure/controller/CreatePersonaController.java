dff47c8a546ce1723dbee502c522c849f377a544
KosMoS7NS <108921638+DanielDiezMiguel@users.noreply.github.com>
2022-08-16 15:23:17 +0200

    EJTE-1
    Fix pom.xml

--- EJTE-1/src/main/java/com/bosonit/persona/infrastructure/controller/CreatePersonaController.java
+++ EJTE-1/src/main/java/com/bosonit/persona/infrastructure/controller/CreatePersonaController.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@CrossOrigin(origins = "*", methods = RequestMethod.POST)
 @RequestMapping("persona")
 public class CreatePersonaController {
 
--- EJTE-1/src/main/java/com/bosonit/persona/infrastructure/controller/ReadPersonaController.java
+++ EJTE-1/src/main/java/com/bosonit/persona/infrastructure/controller/ReadPersonaController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = "*", methods = RequestMethod.GET)
 @RequestMapping("persona")
 public class ReadPersonaController {
 
