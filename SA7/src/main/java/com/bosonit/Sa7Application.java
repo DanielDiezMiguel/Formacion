19165f26fcf2f94823d380d4c80fc8240db4f127
DanielDiezMiguel <daniel.diez@bosonit.com>
2022-08-16 10:37:08 +0200

    SA7
    Ejercicio creado

---
+++ SA7/src/main/java/com/bosonit/Sa7Application.java
@@ -0,0 +1,13 @@
+package com.bosonit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sa7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sa7Application.class, args);
+	}
+
+}
---
+++ SA7/src/test/java/com/bosonit/SA7/Sa7ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bosonit.SA7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sa7ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
