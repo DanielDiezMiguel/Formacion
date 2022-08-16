901a932a01c9c24c3280539af4d4e4f4daa13709
DanielDiezMiguel <daniel.diez@bosonit.com>
2022-08-16 12:42:33 +0200

    SA7
    Ejercicio creado

--- SA7/src/main/java/com/bosonit/Sa7Application.java
+++ SA7/src/main/java/com/bosonit/Sa7Application.java
@@ -6,9 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Sa7Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Sa7Application.class, args);
-		System.out.println("Esto es una prueba");
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Sa7Application.class, args);
+    }
 
 }
