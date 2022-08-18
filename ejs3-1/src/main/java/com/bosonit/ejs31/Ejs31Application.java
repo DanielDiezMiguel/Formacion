866836fbf93a4a011fbd9cf9481a80015b905cc0
DanielDiezMiguel <daniel.diez@bosonit.com>
2022-08-18 10:39:40 +0200

    ejs3-1
    Proyecto creado

---
+++ ejs3-1/src/main/java/com/bosonit/ejs31/Ejs31Application.java
@@ -0,0 +1,13 @@
+package com.bosonit.ejs31;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ejs31Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ejs31Application.class, args);
+	}
+
+}
