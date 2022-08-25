2bc8c6125a004352c288e80aaffdb0040dbe856b
DanielDiezMiguel <daniel.diez@bosonit.com>
2022-08-25 12:48:50 +0200

    ejercicio-final
    Proyecto creado

---
+++ ejercicio-final/src/test/java/com/bosonit/ejerciciofinal/EjercicioFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bosonit.ejerciciofinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjercicioFinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
