c2b37f4a6ea12e962e4ca117b7e20e0831a5ecba
KosMoS7NS <108921638+DanielDiezMiguel@users.noreply.github.com>
2022-08-26 18:50:29 +0200

    ejercio-final
    Eureka configurado

--- ejercicio-final/back-web/src/main/java/com/bosonit/application/BackWebCrearReservaUseCase.java
+++ ejercicio-final/back-web/src/main/java/com/bosonit/application/BackWebCrearReservaUseCase.java
@@ -10,7 +10,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-@Slf4j
 public class BackWebCrearReservaUseCase implements BackWebReservaPort {
 
     @Autowired
