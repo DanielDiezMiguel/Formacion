9f52e62b4626b5caba978b5440df801f9c294062
KosMoS7NS <108921638+DanielDiezMiguel@users.noreply.github.com>
2022-08-26 18:49:54 +0200

    ejercicio-final
    Eureka configurado

--- ejercicio-final/back-web/src/main/java/com/bosonit/application/BackWebCrearReservaUseCase.java
+++ ejercicio-final/back-web/src/main/java/com/bosonit/application/BackWebCrearReservaUseCase.java
@@ -5,10 +5,12 @@ import com.bosonit.domain.BackWebReservaEntity;
 import com.bosonit.infrastructure.controller.dto.BackWebReservaInputDTO;
 import com.bosonit.infrastructure.controller.dto.BackWebReservaOutputDTO;
 import com.bosonit.infrastructure.repository.mongodb.MongoDBRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j
 public class BackWebCrearReservaUseCase implements BackWebReservaPort {
 
     @Autowired
