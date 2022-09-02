e152941ff3883c49acf51182c8d86a1f1ad128a5
DanielDiezMiguel <daniel.diez@bosonit.com>
2022-09-02 12:37:39 +0200

    ejercicio-final
    Modificado BackEmpresaReservaDisponibleOutputDTO

--- ejercicio-final/back-empresa/src/main/java/com/bosonit/infrastructure/reserva_disponible/controller/dto/BackEmpresaReservaDisponibleOutputDTO.java
+++ ejercicio-final/back-empresa/src/main/java/com/bosonit/infrastructure/reserva_disponible/controller/dto/BackEmpresaReservaDisponibleOutputDTO.java
@@ -7,12 +7,14 @@ import java.util.Date;
 
 @Data
 public class BackEmpresaReservaDisponibleOutputDTO {
+    String id_reserva_disponible;
     String ciudad;
     Date fecha;
     Integer numeroPlazas;
 
     public BackEmpresaReservaDisponibleOutputDTO(BackEmpresaReservaDisponibleCollection backEmpresaReservaDisponibleCollection) {
         if (backEmpresaReservaDisponibleCollection == null) return;
+        id_reserva_disponible = backEmpresaReservaDisponibleCollection.getId_reserva_disponible();
         ciudad = backEmpresaReservaDisponibleCollection.getCiudad();
         fecha = backEmpresaReservaDisponibleCollection.getFecha();
         numeroPlazas = backEmpresaReservaDisponibleCollection.getNumeroPlazas();
