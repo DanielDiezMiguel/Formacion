57a9c48fb0c5a4aa2697c02d157cdad51c69bdea
DanielDiezMiguel <daniel.diez@bosonit.com>
2022-09-05 12:13:58 +0200

    ejercicio-final
    Read ciudad disponible back-web refactorizado

--- ejercicio-final/back-web/src/main/java/com/bosonit/application/reserva_disponible/BackWebReservaDisponibleDisponibleReadUseCase.java
+++ ejercicio-final/back-web/src/main/java/com/bosonit/application/reserva_disponible/BackWebReservaDisponibleDisponibleReadUseCase.java
@@ -1,10 +1,8 @@
 package com.bosonit.application.reserva_disponible;
 
 import com.bosonit.application.reserva_disponible.port.BackWebReservaDisponibleReadPort;
-import com.bosonit.domain.reserva.BackWebReservaCollection;
 import com.bosonit.domain.reserva_disponible.BackWebReservaDisponibleCollection;
-import com.bosonit.infrastructure.reserva.controller.dto.BackWebReservaOutputDTO;
-import com.bosonit.infrastructure.reserva.repository.mongodb.MongoDBRepository;
+import com.bosonit.exception.BadRequest;
 import com.bosonit.infrastructure.reserva_disponible.controller.dto.BackWebReservaDisponibleOutputDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
@@ -24,40 +22,44 @@ public class BackWebReservaDisponibleDisponibleReadUseCase implements BackWebRes
     MongoTemplate mongoTemplate;
 
     @Override
-    public ResponseEntity<List<BackWebReservaDisponibleOutputDTO>> getAllReservas(String ciudad, Date fecha, String condicion) {
-        List<BackWebReservaDisponibleOutputDTO> backWebReservaOutputDTOList = new ArrayList<>();
+    public ResponseEntity<List<BackWebReservaDisponibleOutputDTO>>
+    getAllReservas(String ciudad, Date fecha, String condicion) {
 
-        if (condicion != null) {
+        List<BackWebReservaDisponibleOutputDTO> backWebReservaDisponibleOutputDTOList = new ArrayList<>();
+        Query query = new Query();
+
+        try {
             switch (condicion) {
-                case "inferior":
-                    Query inferior = new Query(new Criteria()
-                            .orOperator(
-                                    Criteria.where("ciudad").is(ciudad),
-                                    Criteria.where("fechaMs").lte(fecha.getTime())
-                            ));
-                    mongoTemplate.find(inferior, BackWebReservaDisponibleCollection.class, "reservas-disponibles")
-                            .forEach(backWebReservaCollection -> backWebReservaOutputDTOList
-                                    .add(new BackWebReservaDisponibleOutputDTO(backWebReservaCollection)));
-                    break;
-
                 case "superior":
-                    Query superior = new Query(new Criteria()
-                            .orOperator(
+                    query = new Query(new Criteria()
+                            .andOperator(
                                     Criteria.where("ciudad").is(ciudad),
                                     Criteria.where("fechaMs").gte(fecha.getTime())
                             ));
-                    mongoTemplate.find(superior, BackWebReservaDisponibleCollection.class, "reservas-disponibles")
-                            .forEach(backWebReservaCollection -> backWebReservaOutputDTOList
-                                    .add(new BackWebReservaDisponibleOutputDTO(backWebReservaCollection)));
+                    break;
+
+                case "inferior":
+                    query = new Query(new Criteria()
+                            .andOperator(
+                                    Criteria.where("ciudad").is(ciudad),
+                                    Criteria.where("fechaMs").lte(fecha.getTime())
+                            ));
+                    break;
+
+                case "ciudad":
+                    query = new Query(Criteria.where("ciudad").is(ciudad));
                     break;
             }
 
-        } else {
-            Query query = new Query(Criteria.where("ciudad").is(ciudad));
             mongoTemplate.find(query, BackWebReservaDisponibleCollection.class, "reservas-disponibles")
-                    .forEach(backWebReservaCollection -> backWebReservaOutputDTOList
+                    .forEach(backWebReservaCollection -> backWebReservaDisponibleOutputDTOList
                             .add(new BackWebReservaDisponibleOutputDTO(backWebReservaCollection)));
+
+            return ResponseEntity.ok(backWebReservaDisponibleOutputDTOList);
+
+        } catch (Exception e) {
+            throw new BadRequest("Introduce una condicion: superior, inferior o ciudad");
         }
-        return ResponseEntity.ok(backWebReservaOutputDTOList);
     }
 }
+
--- ejercicio-final/back-web/src/main/java/com/bosonit/application/reserva_disponible/port/BackWebReservaDisponibleReadPort.java
+++ ejercicio-final/back-web/src/main/java/com/bosonit/application/reserva_disponible/port/BackWebReservaDisponibleReadPort.java
@@ -8,6 +8,10 @@ import java.util.Date;
 import java.util.List;
 
 public interface BackWebReservaDisponibleReadPort {
-    ResponseEntity<List<BackWebReservaDisponibleOutputDTO>> getAllReservas(String ciudad, Date fecha, String condicion);
+//    ResponseEntity<List<BackWebReservaDisponibleOutputDTO>> getAllReservas(String ciudad, Date fecha, String condicion);
+//    ResponseEntity<List<BackWebReservaDisponibleOutputDTO>> getAllReservas(
+//            String ciudad, Date fechaInferior, Date fechaSuperior);
 
+    ResponseEntity<List<BackWebReservaDisponibleOutputDTO>> getAllReservas(
+            String ciudad, Date fecha, String condicion);
 }
