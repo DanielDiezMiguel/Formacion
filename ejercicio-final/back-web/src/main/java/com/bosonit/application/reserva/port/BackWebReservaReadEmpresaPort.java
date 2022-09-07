9c7a069018abc485fc8e0328173abdaeaa83f1cf
DanielDiezMiguel <daniel.diez@bosonit.com>
2022-09-07 10:41:02 +0200

    ejercicio-final
    Feign corregido con eureka, parte terminada

--- ejercicio-final/back-web/src/main/java/com/bosonit/application/reserva/port/BackWebReservaReadEmpresaPort.java
+++ ejercicio-final/back-web/src/main/java/com/bosonit/application/reserva/port/BackWebReservaReadEmpresaPort.java
@@ -6,7 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "back-web-feign", url = "http://localhost:8090")
+@FeignClient(name = "BACK-EMPRESA")
 public interface BackWebReservaReadEmpresaPort {
 
     @GetMapping("/api/v0/security/token/{token}")
