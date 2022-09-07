f8e2499c0780a77b11ae7fb248f1f3845a9506e2
DanielDiezMiguel <daniel.diez@bosonit.com>
2022-09-07 10:29:10 +0200

    ejercicio-final
    Comentarios refactorizados

--- ejercicio-final/back-web/src/main/java/com/bosonit/infrastructure/reserva/controller/BackWebReservaReadEmpresaController.java
+++ ejercicio-final/back-web/src/main/java/com/bosonit/infrastructure/reserva/controller/BackWebReservaReadEmpresaController.java
@@ -29,38 +29,15 @@ public class BackWebReservaReadEmpresaController {
             @PathVariable(name = "token") String token,
             @RequestParam(value = "ciudad", required = false) String ciudad,
             @RequestParam(value = "condicion", required = false) String condicion,
-            @RequestParam(value = "fecha", required = false) @DateTimeFormat(pattern = "yyyy-MM-dd") Date fecha) throws Exception {
+            @RequestParam(value = "fecha", required = false)
+            @DateTimeFormat(pattern = "yyyy-MM-dd") Date fecha) throws Exception {
 
         try {
             backWebReservaReadEmpresaPort.getToken(token);
             return backWebReservaReadPort.getAllReservas(ciudad, fecha, condicion);
+
         } catch (Exception e) {
             throw new Forbidden("Token invalido");
         }
     }
-
-//    @GetMapping("{token}")
-//    public ResponseEntity getToken(@PathVariable(name = "token") String token) {
-//        try {
-//            return backWebReservaReadEmpresaPort.getToken(token);
-//        } catch (Exception e) {
-//            throw new Forbidden("Token invalido");
-//        }
-//    }
-
-//    @RestController
-//    @RequestMapping("/api/v0/reserva")
-//    public class BackWebReservaReadController {
-//
-//        @Autowired
-//        BackWebReservaReadPort backWebReservaReadPort;
-//
-//        @GetMapping
-//        public ResponseEntity<List<BackWebReservaOutputDTO>> getAllReservas(
-//                @RequestParam(value = "ciudad", required = false) String ciudad,
-//                @RequestParam(value = "condicion", required = false) String condicion,
-//                @RequestParam(value = "fecha", required = false) @DateTimeFormat(pattern = "yyyy-MM-dd") Date fecha) {
-//            return backWebReservaReadPort.getAllReservas(ciudad, fecha, condicion);
-//        }
-//    }
 }
