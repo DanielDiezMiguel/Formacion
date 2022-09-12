13755d22b3617822a569fbf44553fcec07af63ac
DanielDiezMiguel <daniel.diez@bosonit.com>
2022-09-12 09:30:18 +0200

    DBA3
    Proyecto creado

---
+++ DBA3/src/main/java/com/bosonit/Dba3Application.java
@@ -0,0 +1,13 @@
+package com.bosonit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dba3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dba3Application.class, args);
+	}
+
+}
---
+++ DBA3/src/test/java/com/bosonit/DBA3/Dba3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bosonit.DBA3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dba3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
