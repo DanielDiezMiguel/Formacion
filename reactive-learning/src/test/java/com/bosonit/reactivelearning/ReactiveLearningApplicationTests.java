6a5a5db0a36599f912e8baf58941343eef7c374c
DanielDiezMiguel <daniel.diez@bosonit.com>
2022-09-15 13:03:29 +0200

    reactive-learning
    Tests refactorizados y terminados

--- reactive-learning/src/test/java/com/bosonit/reactivelearning/ReactiveLearningApplicationTests.java
+++ reactive-learning/src/test/java/com/bosonit/reactivelearning/ReactiveLearningApplicationTests.java
@@ -1,13 +1,21 @@
 package com.bosonit.reactivelearning;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.reactive.server.WebTestClient;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
 @SpringBootTest
+@AutoConfigureWebTestClient
 class ReactiveLearningApplicationTests {
+
+    @Autowired
+    WebTestClient webTestClient;
 
     @Test
     void fluxTextOnError() {
@@ -84,5 +92,40 @@ class ReactiveLearningApplicationTests {
                         .log())
                 .expectError(RuntimeException.class)
                 .verify();
+    }
+
+    @Test
+    void main() {
+        ReactiveLearningApplication.main(new String[]{});
+    }
+
+    @Test
+    void integerFlux() {
+        Flux<Integer> integerFlux = webTestClient.get().uri("/flux")
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus().isOk()
+                .returnResult(Integer.class)
+                .getResponseBody();
+
+        StepVerifier.create(integerFlux)
+                .expectSubscription()
+                .expectNext(1, 2, 3, 4, 5)
+                .verifyComplete();
+    }
+
+    @Test
+    void integerStreamFlux() {
+        Flux<Integer> integerStreamFlux = webTestClient.get().uri("/flux/stream")
+                .accept(MediaType.valueOf(MediaType.TEXT_EVENT_STREAM_VALUE))
+                .exchange()
+                .expectStatus().isOk()
+                .returnResult(Integer.class)
+                .getResponseBody();
+
+        StepVerifier.create(integerStreamFlux)
+                .expectSubscription()
+                .expectNext(1, 2, 3, 4, 5)
+                .verifyComplete();
     }
 }
