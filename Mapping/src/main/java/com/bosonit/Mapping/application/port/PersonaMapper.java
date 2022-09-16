e3db7bee31e71865f0ba561e9296a2ff747ba0d2
DanielDiezMiguel <daniel.diez@bosonit.com>
2022-09-16 12:53:55 +0200

    Mapping
    Ejercicio terminado

--- Mapping/src/main/java/com/bosonit/Mapping/application/port/PersonaMapper.java
+++ Mapping/src/main/java/com/bosonit/Mapping/application/port/PersonaMapper.java
@@ -9,6 +9,7 @@ import org.mapstruct.factory.Mappers;
 
 /**
  * Se realiza la conversión de DTO
+ *
  * @author Daniel Díez Miguel
  */
 @Mapper
@@ -16,15 +17,7 @@ public interface PersonaMapper {
 
   PersonaMapper INSTANCE = Mappers.getMapper(PersonaMapper.class);
 
-  @Mapping(target = "id", source = "id")
-  @Mapping(target = "nombre", source = "nombre")
-  @Mapping(target = "apellidos", source = "apellidos")
-  @Mapping(target = "edad", source = "edad")
   PersonaEntity persona(PersonaInputDTO personaInputDTO);
 
-  @Mapping(target = "id", source = "id")
-  @Mapping(target = "nombre", source = "nombre")
-  @Mapping(target = "apellidos", source = "apellidos")
-  @Mapping(target = "edad", source = "edad")
   PersonaOutputDTO personaOutputDto(PersonaEntity personaEntity);
 }
